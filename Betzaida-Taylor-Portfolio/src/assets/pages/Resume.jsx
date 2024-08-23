e144a9ae2c8578f5619ef05619bd41c6f7e77279
Betzaida Taylor <156623909+Betzaida96@users.noreply.github.com>
2024-08-23 01:30:19 -0600

    resume section

---
+++ Betzaida-Taylor-Portfolio/src/assets/pages/Resume.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Resume = () => {
+    return (
+        <section>
+            <h2>Resume</h2>
+            <p>
+                You can download my resume <a href="/Betzaida-Taylor-Resume.pdf" download>here</a>.
+            </p>
+            <h3>Proficiencies</h3>
+            <ul>
+              <li>JavaScript, HTML5, CSS3, React, Node.js, Express.js, PostgreSQL</li>
+              <li>Front-End Development: Responsive Design, Cross-Browser Compatibility</li>
+              <li>Back-End Development: RESTful APIs, Database Design</li>
+              <li>Version Control: Git, GitHub</li>
+              <li>Soft Skills: Excellent customer service, strong communications skills, Team collaboration</li>
+            </ul>
+        </section>
+    );
+};
+
+export default Resume;
