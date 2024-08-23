7b92a54398218367ec970c1b202e2f8612116426
Betzaida Taylor <156623909+Betzaida96@users.noreply.github.com>
2024-08-23 13:13:58 -0600

    restyling the header

--- Betzaida-Taylor-Portfolio/src/components/Header.jsx
+++ Betzaida-Taylor-Portfolio/src/components/Header.jsx
@@ -4,15 +4,18 @@ import {Link} from 'react-router-dom'
 const Header = () => {
     return(
         <header>
-            <h1>Betzaida Taylor</h1>
+            <div className="container mx-auto flex justify-between items-center">
+            <h1 className="text-7xl font-bold text-pinkyRed">Betzaida Taylor</h1>
             <nav>
-                <ul>
-                    <li><Link to="/">About Me</Link></li>
-                    <li><Link to="/Portfolio">Portfolio</Link></li>
-                    <li><Link to="/Contact">Contact</Link></li>
-                    <li><Link to="/Resume">Resume</Link></li>
+                <ul className="flex space-x-2 p-4">
+                    <li><Link to="/" className={`px-8 py-2 rounded-t-lg
+                        ${location.pathname === '/' ? 'bg-lavender text-2xl text-white' : 'text-pinkyRed hover:bg-lavender hover:text-pinkyRed'}`}>About Me</Link></li>
+                    <li><Link to="/Portfolio" className={`px-8 py-2 rounded-t-lg ${location.pathname === '/' ? 'bg-lavender text-2xl text-white' : 'hover:bg-lavender hover:text-pinkyRed'}`}>Portfolio</Link></li>
+                    <li><Link to="/Contact" className={`px-8 py-2 rounded-t-lg ${location.pathname === '/' ? 'bg-lavender text-2xl text-white' : 'hover:bg-lavender hover:text-pinkyRed'}`}>Contact</Link></li>
+                    <li><Link to="/Resume" className={`px-8 py-2 rounded-t-lg ${location.pathname === '/' ? 'bg-lavender text-2xl text-white' : 'hover:bg-lavender hover:text-pinkyRed'}`}>Resume</Link></li>
                 </ul>
             </nav>
+            </div>
         </header>
     );
 };
--- Betzaida-Taylor-Portfolio/tailwind.config.js
+++ Betzaida-Taylor-Portfolio/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./src/**/*.{js, jsx, ts, tsx}', './public/index.html'],
-  darkMode: false,
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
+    colors: {
+      lightLightPink: '#fce7f3',
+      rosePink: '#f472b6',
+      lavender: '#e879f9',
+      brightLavender: '#d946ef',
+      pinkyRed: '#e11d48',
+      darkRed: '#500724',
+      white: '#ffff'
+    },
     extend: {},
   },
   varients: {
